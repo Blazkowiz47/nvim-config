be3001b4f7f3605c22213ec911aa6370e5f33cb6
Blazkowiz47 <sushrutpatwardhan@gmail.com>
2024-10-06 06:19:13 +0000

    updates

---
+++ initialise.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+ln -s $HOME/.config/nvim/tmux.conf $HOME/tmux.conf
+ln -s $HOME/.config/nvim/wezterm $HOME/.config/wezterm
+
+
+
+
