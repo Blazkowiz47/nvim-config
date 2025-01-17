fb7feb8e997dbb65dce502d78dc3f1f7b45bb3c1
blazkowiz <sushrut.patwardhan@mobai.bio>
2025-01-17 06:38:21 +0100

    Path autocomplete updates

--- lua/blazkowiz47/lazy/lsp.lua
+++ lua/blazkowiz47/lazy/lsp.lua
@@ -89,6 +89,7 @@ return {
             sources = cmp.config.sources({
                 { name = "nvim_lsp" },
                 { name = "luasnip" },
+                { name = "path" },
                 -- { name = "friendly-snippets" },
             }, {
                 { name = "buffer" },
