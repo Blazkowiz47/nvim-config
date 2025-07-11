68a06db160cd557b928b318d320d9a8ef9d3407a
blazkowiz47 <sushrutpatwardhan@gmail.com>
2025-07-11 08:27:18 +0200

    updates

--- _after/plugin/blink.lua
+++
@@ -1,7 +0,0 @@
-local capabilities = require("blink.cmp").get_lsp_capabilities()
-
-require("lspconfig").lua_ls.setup({ capabilities = capabilities })
-require("lspconfig").ruff.setup({ capabilities = capabilities })
-require("lspconfig").pyright.setup({ capabilities = capabilities })
-
-print("Setting capabilities")
