7f56412dea3003f0a110d34d131c8cb4409e1baf
Ubuntu <ubuntu@mobai2.openstack.local>
2025-08-18 15:13:51 +0200

    updatest

--- initialise.sh
+++ initialise.sh
@@ -5,6 +5,7 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim\
 
 ln -s $HOME/.config/nvim/tmux.conf $HOME/tmux.conf
 ln -s $HOME/.config/nvim/wezterm $HOME/.config/wezterm
+ln -s $HOME/.config/nvim/zellij $HOME/.config/zellij
 
 
 
--- lua/blazkowiz47/lazy/markview.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "OXY2DEV/markview.nvim",
-    lazy = false,
-    config = function()
-        vim.keymap.set("n", "<leader>mt", "<cmd>Markview toggle<CR>")
-    end,
-}
