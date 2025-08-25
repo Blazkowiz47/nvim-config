f82e6431d532affc212601f3bfcbf29a99baac6a
blazkowiz47 <sushrutpatwardhan@gmail.com>
2025-08-25 17:09:27 +0200

    added stow

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 For Linux:
 ```
-    git clone https://github.com/github/copilot.vim.git ~/.config/nvim/pack/github/start/copilot.vim
-    sudo luarocks install jsregexp
+    sh initialise.sh
+
 ```
 
 For windows:
--- initialise.sh
+++ initialise.sh
@@ -4,9 +4,10 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 git clone https://github.com/github/copilot.vim.git ~/.config/nvim/pack/github/start/copilot.vim
 
-sudo -y apt install luarocks fd-find ripgrep stow
-sudo npm install -g tree-sitter-cli
-sudo -y luarocks install jsregexp
-
+sudo -y apt install stow
 stow --no-folding -vt ~ .
 
+# Extra supporting packages
+sudo -y apt install luarocks fd-find ripgrep
+sudo -y luarocks install jsregexp
+sudo npm install -g tree-sitter-cli
