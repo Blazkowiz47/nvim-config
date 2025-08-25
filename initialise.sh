39bf8aac19b4285068dcc02ca530f6906df6c7ca
blazkowiz47 <sushrutpatwardhan@gmail.com>
2025-08-25 17:04:07 +0200

    updated dotfiles config

--- initialise.sh
+++ initialise.sh
@@ -4,6 +4,9 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 git clone https://github.com/github/copilot.vim.git ~/.config/nvim/pack/github/start/copilot.vim
 
+sudo -y apt install luarocks fd-find ripgrep stow
 sudo npm install -g tree-sitter-cli
-sudo -y apt install luarocks fd-find ripgrep
 sudo -y luarocks install jsregexp
+
+stow --no-folding -vt ~ .
+
