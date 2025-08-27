568e57bb0626535b77e059b931a74a0731fb0ab1
Ubuntu <ubuntu@mobai2.openstack.local>
2025-08-27 13:23:47 +0200

    updates

--- initialise.sh
+++ initialise.sh
@@ -4,10 +4,10 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 git clone https://github.com/github/copilot.vim.git ~/.config/nvim/pack/github/start/copilot.vim
 
-sudo -y apt install stow
+sudo apt install stow -y
 stow --no-folding -vt ~ .
 
 # Extra supporting packages
-sudo -y apt install luarocks fd-find ripgrep
-sudo -y luarocks install jsregexp
+sudo apt install luarocks fd-find ripgrep -y 
+sudo luarocks install jsregexp 
 sudo npm install -g tree-sitter-cli
