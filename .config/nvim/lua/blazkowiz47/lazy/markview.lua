083cf63e899222b86c2eb8e5b044a2e8f241f651
sushrutpatwardhan@gmail.com <sushrutpatwardhan@Sushruts-MacBook-Pro.local>
2025-10-27 10:48:51 +0100

    chore: added remap for markview toggle

--- .config/nvim/lua/blazkowiz47/lazy/markview.lua
+++ .config/nvim/lua/blazkowiz47/lazy/markview.lua
@@ -2,6 +2,9 @@ return {
     "OXY2DEV/markview.nvim",
     lazy = false,
 
+    config = function()
+        vim.keymap.set("n", "<leader>mt", ":Markview toggle<CR>")
+    end,
     -- For blink.cmp's completion
     -- source
     -- dependencies = {
